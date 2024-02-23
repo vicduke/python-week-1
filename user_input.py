783c05eabbfec00c094a541134a94e449bde5c0a
victor <mutaivic10@gmail.com>
2024-02-23 12:53:58 +0300

    �python assignment week 1

---
+++ user_input.py
@@ -0,0 +1,4 @@
+name=input("What is your name?")
+age=input("What is your age?")
+location=input("Where do you live?")
+print(f"Helllo {name}.You are {age} years old.Your residence is {location}.Have a nice day!")
